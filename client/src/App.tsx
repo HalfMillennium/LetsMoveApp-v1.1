c9da4a88c38440df81cd8ae15cc947090e22aa7c
Garrett Chestnut <glc2266@gmail.com>
2025-04-10 00:12:27 +0000

    Update layout of Listings page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,6 @@ import Header from "./components/Header";
 import MobileNavBar from "./components/MobileNavBar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import Listings from "./pages/Listings";
 import Listings2 from "./pages/Listings2";
 import SearchParty from "./pages/SearchParty";
 import Favorites from "./pages/Favorites";
@@ -17,8 +16,7 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/listings" component={Listings} />
-      <Route path="/listings2" component={Listings2} />
+      <Route path="/listings" component={Listings2} />
       <Route path="/search-party" component={SearchParty} />
       <Route path="/favorites" component={Favorites} />
       <Route path="/profile" component={Profile} />
@@ -34,7 +32,7 @@ function AppContent() {
   return (
     <div className="flex flex-col min-h-screen">
       {!isListings2Page && <Header />}
-      <main className={`flex-1 ${isListings2Page ? '' : 'flex'}`}>
+      <main className={`flex-1 ${isListings2Page ? "" : "flex"}`}>
         <Router />
       </main>
       {!isListings2Page && <Footer />}
